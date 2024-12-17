8c9e6fbe3647d878a7d6826688d9c587fb83e232
Minh Nghĩa <109430898+minhnghia1103@users.noreply.github.com>
2024-12-18 06:49:43 +0700

    change version

--- main.py
+++ main.py
@@ -148,7 +148,7 @@ def test(epoch):
         best_acc = acc
 
 
-for epoch in range(start_epoch, start_epoch+10):
+for epoch in range(start_epoch, start_epoch+1):
     train(epoch)
     test(epoch)
     scheduler.step()
