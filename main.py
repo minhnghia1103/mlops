365c78aa2b9bf593f18842d18cc0a96c56e1a056
Minh Nghĩa <109430898+minhnghia1103@users.noreply.github.com>
2024-12-29 04:23:32 +0700

    fix

--- main.py
+++ main.py
@@ -44,13 +44,11 @@ transform_test = transforms.Compose([
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
-trainset = torchvision.datasets.CIFAR10(
-    root='./data', train=True, download=True, transform=transform_train)
+trainset = torch.load('data/trainset.pt')
 trainloader = torch.utils.data.DataLoader(
     trainset, batch_size=128, shuffle=True, num_workers=0)
 
-testset = torchvision.datasets.CIFAR10(
-    root='./data', train=False, download=True, transform=transform_test)
+testset = torch.load('data/testset.pt')
 testloader = torch.utils.data.DataLoader(
     testset, batch_size=100, shuffle=False, num_workers=0)
 
